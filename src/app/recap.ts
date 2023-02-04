7663c2d8ecbc00f1a9c9a278c40c1c75e48824fb
josfergrost <josfergrost@gmail.com>
2023-02-04 16:49:51 -0600

    Update

---
+++ src/app/recap.ts
@@ -0,0 +1,17 @@
+import { last } from "rxjs";
+
+const username:  string | number = 'ShiroTsurugi';
+const sum = (a:number, b: number)=>{
+  return a+b;
+}
+sum(1,2)
+class Person{
+
+
+  constructor(public age: number,public lastName:string){
+
+  }
+
+}
+const nico = new Person(15,'Molina');
+nico.age;
